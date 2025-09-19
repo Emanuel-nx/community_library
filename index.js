5759c74d2112bf390f5f4bcefdc736ab6c5e5ca0
Emanuel-nx <emanuel.nx@hotmail.com>
2025-09-19 17:13:06 -0300

    feat: service, controllers, routes. Users

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 import express, { json } from 'express'
-const app = express()
+import userRouters from './src/routes/user.routes.js'
+const app = express();
 
 app.use(express.json());
-const users = []
-
-app.get("/hello", (req, res) => {
-  res.send('Hello World')
-})
-
-// METODO => GET, POST, PUT/PATCH, DELETE
-// NOME => "Começa com /", sempre deve ser no plural. Exemplo: /users
-// Callback functions => Onde executamos o backend (lógica, regra de négocio ...) 
-
-app.post("/users", (req, res) => {
-    const body = req.body
-    users.push(body)
-    res.status(201).send("Usuario criado com sucesso!");
-});
-
-app.get("/users", (req, res) => {
-    res.send({users})
-});
+app.use(userRouters)
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
